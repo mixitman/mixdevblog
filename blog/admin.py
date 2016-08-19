d4990854c9eeb8c134c47a8b568adf8cab6582f5
mixitman <handonfist@gmail.com>
2016-08-19 00:34:29 -0500

    working

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "author", "created_date", "published_date"]
+
+admin.site.register(Post, BlogAdmin)
